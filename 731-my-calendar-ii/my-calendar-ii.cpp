42c98d17cec4a9dbe42e35d82730a52bdaf509cd
Anshuman dubey <dubeyanshuman831@gmail.com>
2024-09-27 14:33:09 +0530

    Time: 55 ms (99.48%) | Memory: 37.9 MB (90.93%) - LeetSync

---
+++ 731-my-calendar-ii/my-calendar-ii.cpp
@@ -0,0 +1,31 @@
+class MyCalendarTwo {
+public:
+    vector<pair<int, int>> ans;
+    vector<pair<int, int>> overlap_book;
+
+    MyCalendarTwo() {
+        
+    }
+
+    bool book(int start, int end) {
+        for (auto &overlap : overlap_book) {
+            if (max(start, overlap.first) < min(end, overlap.second)) {
+                return false;
+            }
+        }
+
+        for (auto &val : ans) {
+            if (max(start, val.first) < min(end, val.second)) {
+                overlap_book.push_back({max(start, val.first), min(end, val.second)});
+            }
+        }
+        ans.push_back({start, end});
+        return true;
+    }
+};
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo* obj = new MyCalendarTwo();
+ * bool param_1 = obj->book(start,end);
+ */
